5c5d83586d8d7c75b7f72f4d375e5f7a2670ceb7
Geetansh Gupta <67698473+geetansh73@users.noreply.github.com>
2021-01-21 19:36:09 +0530

    Add files via upload

---
+++ priorityqueue.java
@@ -0,0 +1,22 @@
+import java.util.*;  
+class priorityqueue{  
+public static void main(String args[]){  
+PriorityQueue<String> queue=new PriorityQueue<String>();  
+queue.add("E");  
+queue.add("B");  
+queue.add("C");  
+queue.add("D");  
+queue.add("A"); 
+System.out.println(queue);
+System.out.println("head:"+queue.element());  
+System.out.println("head:"+queue.peek());  
+System.out.println("iterating the queue elements:");  
+Iterator itr=queue.iterator();  
+while(itr.hasNext()){
+System.out.println(itr.next());  }
+queue.remove();
+queue.poll();  
+System.out.println("after removing two elements:");  
+Iterator<String> itr2=queue.iterator();  
+while(itr2.hasNext()){  
+System.out.println(itr2.next());  }}}  
